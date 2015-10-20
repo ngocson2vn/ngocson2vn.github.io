d2620a4c8ba7feb8ac9f75255271720980a1bceb
Son Nguyen <ngocson2vn@gmail.com>
2015-10-20 16:35:46 +0900

    Update

---
+++ truncate_table.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+import sys
+import sqlite3
+
+conn = sqlite3.connect("jpvocab.db")
+c = conn.cursor()
+c.execute("DROP TABLE vocab")
+c.execute("CREATE TABLE vocab(id integer primary key, kanji text NOT NULL, hiragana text NOT NULL, en text NOT NULL, vn text NOT NULL)")
+conn.commit()
