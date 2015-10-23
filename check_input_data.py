095a0d842dff1ee12fda13f0a4995cfcf84d52b6
Son Nguyen <ngocson2vn@gmail.com>
2015-10-23 19:17:05 +0900

    Update

--- check_input_data.py
+++ check_input_data.py
@@ -7,7 +7,7 @@ import re
 input_file = sys.argv[1]
 
 f = open(input_file)
-#fw = open(input_file + '.fixed', 'w')
+fw = open(input_file + '.fixed', 'w')
 
 i = 1
 line = f.readline()
@@ -16,9 +16,9 @@ while line:
 	words = line.split("#")
 	
 	if len(words[1]) < len(words[0]) and words[1] != '-':
-		print "line %d: %s" % (i, words[0])
+		print "=>line %d: %s" % (i, words[0])
 	elif words[0] == words[1]:
-		print "line %d: %s" % (i, words[0])
+		print "==>line %d: %s" % (i, words[0])
 		words[1] = '-'
 	else:
 		lw = len(words[0].decode('utf8'))
@@ -26,14 +26,15 @@ while line:
 		for uc in words[0].decode('utf8'):
 			if not re.search(u'[\u4e00-\u9faf]', uc):
 				k = k + 1
-		if k == lw:
-			print "line %d: %s" % (i, words[0])
+		if k == lw and words[1] != '-':
+			print "===>line %d: %s" % (i, words[0])
+			words[1] = '-'
 
-	#fw.write("%s#%s#%s#%s" % (words[0], words[1], words[2], words[3]))
+	fw.write("%s#%s#%s#%s" % (words[0], words[1], words[2], words[3]))
 	
 	i = i + 1
 	line = f.readline()
 
 f.close()
-#fw.close()
-#shutil.move(input_file, 'backup')
+fw.close()
+shutil.move(input_file, 'backup')
